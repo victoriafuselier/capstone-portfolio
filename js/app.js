fe98619822bdc77510b4b9cc4998d433227292f8
Victoria Fuselier <vic.fuselier@gmail.com>
2023-07-24 23:43:36 -0500

    Programmed about section up and down arrows
    Still need to fix the rotate issue on hover

--- js/app.js
+++ js/app.js
@@ -1,15 +1,47 @@
-const dropArrow = document.querySelector('#dropdown-arrow');
+const dropdownArrowBackground = document.querySelector('#dropdown-arrow-background');
 
-dropArrow.addEventListener('click', () => {
-    const textToDisplay = dropArrow.parentNode.nextElementSibling;
+dropdownArrowBackground.addEventListener('click', () => {
+    const textToDisplay = dropdownArrowBackground.parentNode.nextElementSibling;
     textToDisplay.style.display = 'block';
-    dropArrow.style.display = 'none';
+    dropdownArrowBackground.style.display = 'none';
 });
 
-const upArrow = document.querySelector('.up-arrow');
+const upArrowBackground = document.querySelector('#up-arrow-background');
 
-upArrow.addEventListener('click', () => {
-    const textToHide = upArrow.parentNode;
+upArrowBackground.addEventListener('click', () => {
+    const textToHide = upArrowBackground.parentNode;
     textToHide.style.display = 'none';
-    dropArrow.style.display = 'block';
+    dropdownArrowBackground.style.display = 'block';
+});
+
+const dropdownArrowEducation = document.querySelector('#dropdown-arrow-education');
+
+dropdownArrowEducation.addEventListener('click', () => {
+    const textToDisplay = dropdownArrowEducation.parentNode.nextElementSibling;
+    textToDisplay.style.display = 'block';
+    dropdownArrowEducation.style.display = 'none';
+});
+
+const upArrowEducation = document.querySelector('#up-arrow-education');
+
+upArrowEducation.addEventListener('click', () => {
+    const textToHide = upArrowEducation.parentNode;
+    textToHide.style.display = 'none';
+    dropdownArrowEducation.style.display = 'block';
+});
+
+const dropdownArrowSkills = document.querySelector('#dropdown-arrow-skills');
+
+dropdownArrowSkills.addEventListener('click', () => {
+    const textToDisplay = dropdownArrowSkills.parentNode.nextElementSibling;
+    textToDisplay.style.display = 'block';
+    dropdownArrowSkills.style.display = 'none';
+});
+
+const upArrowSkills = document.querySelector('#up-arrow-skills');
+
+upArrowSkills.addEventListener('click', () => {
+    const textToHide = upArrowSkills.parentNode;
+    textToHide.style.display = 'none';
+    dropdownArrowSkills.style.display = 'block';
 });
