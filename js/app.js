9d6ee8ae41eb1e26c97f679a50a86973d6879083
Victoria Fuselier <vic.fuselier@gmail.com>
2023-07-25 10:59:02 -0500

    Fixed accessibility issues
    Noticed that I didn't have tab functionality because arrows had p tags instead of buttons and photo overlay was only accessible via mouse on hover. Fixed all!
    
    Making reminder to go back and add tabindex to footer elements when I add in my SVGs for contact info, social media, etc

--- js/app.js
+++ js/app.js
@@ -5,42 +5,42 @@ const upArrowEducation = document.querySelector('#up-arrow-education');
 const dropdownArrowSkills = document.querySelector('#dropdown-arrow-skills');
 const upArrowSkills = document.querySelector('#up-arrow-skills');
 
-dropdownArrowBackground.addEventListener('click', () => {
+dropdownArrowBackground.addEventListener('focus', () => {
     const textToDisplay = dropdownArrowBackground.parentNode.nextElementSibling;
     textToDisplay.style.display = 'block';
     dropdownArrowBackground.style.display = 'none';
     upArrowBackground.style.display = 'block'
 });
 
-upArrowBackground.addEventListener('click', () => {
+upArrowBackground.addEventListener('focus', () => {
     const textToHide = upArrowBackground.parentNode.nextElementSibling;
     textToHide.style.display = 'none';
     dropdownArrowBackground.style.display = 'block';
     upArrowBackground.style.display = 'none';
 });
 
-dropdownArrowEducation.addEventListener('click', () => {
+dropdownArrowEducation.addEventListener('focus', () => {
     const textToDisplay = dropdownArrowEducation.parentNode.nextElementSibling;
     textToDisplay.style.display = 'block';
     dropdownArrowEducation.style.display = 'none';
     upArrowEducation.style.display = 'block';
 });
 
-upArrowEducation.addEventListener('click', () => {
+upArrowEducation.addEventListener('focus', () => {
     const textToHide = upArrowEducation.parentNode.nextElementSibling;
     textToHide.style.display = 'none';
     dropdownArrowEducation.style.display = 'block';
     upArrowEducation.style.display = 'none';
 });
 
-dropdownArrowSkills.addEventListener('click', () => {
+dropdownArrowSkills.addEventListener('focus', () => {
     const textToDisplay = dropdownArrowSkills.parentNode.nextElementSibling;
     textToDisplay.style.display = 'block';
     dropdownArrowSkills.style.display = 'none';
     upArrowSkills.style.display = 'block';
 });
 
-upArrowSkills.addEventListener('click', () => {
+upArrowSkills.addEventListener('focus', () => {
     const textToHide = upArrowSkills.parentNode.nextElementSibling;
     textToHide.style.display = 'none';
     dropdownArrowSkills.style.display = 'block';
