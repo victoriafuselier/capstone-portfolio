59e79a45c1c1b808d6000472d48b139f73948358
Victoria Fuselier <vic.fuselier@gmail.com>
2024-03-14 21:56:33 -0500

    Changed text content, overlay button, other styling

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ dropdownArrowBackground.addEventListener('click', () => {
 
     popUpDiv.innerHTML = `<div id='pop-up-box'>
                             <button class="close">x</button>
-                            <p>I'm an aspiring web developer in search of an entry-level position in front-end web development. I really enjoy designing beautiful web pages and letting my perfectionist <em>and</em> my creative side run wild with layout, formatting, color schemes, aesthetics, fun JavaScript functionality, etc.</p>
-                            <p>I'm striving for a career that will be interesting, stable, challenging, and fulfilling. My strengths include straightforward and respectful communication, grammar and writing proficiency, and a good eye for design.</p>
-                            <p>I look forward to working in a team-oriented, fun, and challenging workplace environment. If I sound like a good fit for your team, please add me to your contacts list and feel free to shoot me a message below!</p>
+                            <p>I'm an aspiring web developer in search of an entry-level position in front-end web development.</p>
+                            <p>My strengths include a good eye for design, exceptional grammar/writing skills, time-management proficiency, and great teamwork capabilities.</p>
+                            <p>If I sound like a good fit for your team, please send a message below!</p>
                         </div>`;
 
     let closeButton = document.querySelector('.close');
@@ -31,10 +31,10 @@ dropdownArrowEducation.addEventListener('click', () => {
     popUpDiv.innerHTML = `<div id='pop-up-box'>
                             <button class="close">x</button>
                             <ul id="education">
-                                <li>Louisiana State University: Bachelor's Degree</li>
+                                <li>Bachelor's Degree, Louisiana State University</li>
                                 <li>FreeCodeCamp</li>
-                                <li>Treehouse: Front-End Web Development Track</li>
-                                <li>Treehouse: Front-End Web Development Techdegree</li>
+                                <li>Front-End Web Development Track, Treehouse</li>
+                                <li>Front-End Web Development Techdegree, Treehouse</li>
                             </ul>
                         </div>`;
 
@@ -100,7 +100,7 @@ dropdownArrowSkills.addEventListener('click', () => {
                                             <li>SQL</li>
                                         </ul>
                                 </ul>
-                                <p>Check out my <a tabindex="0" href="https://teamtreehouse.com/profiles/victoriafuselier" target="_blank">Treehouse Profile</a> to see what I've been up to and what I'm currently learning!</p>
+                                <p>Check out my <a tabindex="0" href="https://teamtreehouse.com/profiles/victoriafuselier" target="_blank">Treehouse Profile</a> to see what I've been up to and what I'm currently learning.</p>
                             </div>`;
 
     let closeButton = document.querySelector('.close');
